0f29e61c419f1f9e8a8857f6684567ab4da17709
mathmaster12 <36994858+mathmaster12@users.noreply.github.com>
2019-02-20 16:27:21 -0700

    Add files via upload

---
+++ CSCI442_assignment2.2.py
@@ -0,0 +1,62 @@
+#Marielle Korringa, CSCI 442, 15 Feb. 2019
+#Part two of assignment 2
+
+import numpy as np
+import cv2
+import imutils
+
+#Set the variable of stationary to none
+stationary = None
+
+cap = cv2.VideoCapture(0) #Capture live video from Webcam
+cv2.namedWindow("Image") #Create window called Image
+cv2.namedWindow("Gray")#Create window called Gray
+cv2.namedWindow("Channel Image")#Create window called Channel Image
+cv2.namedWindow("Abs Diff")#Create window called Abs Diff
+
+while(1):
+    # Take each frame from webcam
+    status, frame = cap.read()
+
+    #set the variable of motion to 0
+    motion = 0
+
+    #Convert the video from BGR to GRAY and then blur the image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21,21), 0)
+
+    # if first time through while loop set stationary to gray
+    if stationary is None:
+        stationary = gray
+        continue
+
+    #check for differences between the current gray frame and the stationary frame
+    absdiff_frame = cv2.absdiff(stationary,gray)
+    thresh_frame = cv2.threshold(absdiff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
+
+    #find contour of image
+    cnts = cv2.findContours(thresh_frame.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        motion = 1
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+    
+    cv2.imshow("Image",frame)  #display original video
+    cv2.imshow("Gray",gray) #display video in Gray scale
+    cv2.imshow("Channel Image", thresh_frame) #display video in HSV
+    cv2.imshow("Abs Diff", absdiff_frame) #display video that is the absolute difference of two frames
+
+    #Set stationary as the current gray frame 
+    stationary = gray
+    
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+# Release the capture and destroy all the windows when program ends
+cap.release()
+cv2.destroyAllWindows()
---
+++ CSCI442_assignment2.py
@@ -0,0 +1,88 @@
+#Marielle Korringa, CSCI 442, 15 Feb. 2019
+#Part one of assingment 2
+
+import numpy as np
+import cv2
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0) #Capture live video from Webcam
+cv2.namedWindow("Video") #Create window called Video
+cv2.namedWindow("HSV") #Create window called HSV
+cv2.namedWindow("Black+White") #Create window called Black+White
+
+#Minimum and maximum value for HUE in HSV
+minH = 0
+maxH = 179 
+
+#Minimum and maximum value for SATURATION in HSV
+minS = 0 
+maxS = 255
+
+#Minimum and maximum value for VALUE in HSV
+minV = 0
+maxV = 255
+
+#Create trackbars for the minimum and maximum value for HUE in HSV
+cv2.createTrackbar('lowH','HSV',minH,179,nothing)
+cv2.createTrackbar('highH','HSV',maxH,179,nothing)
+
+#Create trackbars for the minimum and maximum value for SATURATION in HSV
+cv2.createTrackbar('lowS','HSV',minS,255,nothing)
+cv2.createTrackbar('highS','HSV',maxS,255,nothing)
+
+#Create trackbars for the minimum and maximum value for VALUE in HSV
+cv2.createTrackbar('lowV','HSV',minV,255,nothing)
+cv2.createTrackbar('highV','HSV',maxV,255,nothing)
+
+#On left mouse click of HSV display location and values of H, S, and V
+def find_Values(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        px = hsv[y,x]    
+        print ("You clicked at [",y,",",x,"]: H is",px[0],",","S is",px[1],",","V is",px[2],".")
+        
+#Set mouse click event
+cv2.setMouseCallback('HSV',find_Values) 
+
+while(1):
+    # Take each frame from webcam
+    status, frame = cap.read()
+
+    #Get the trackbars position for the minimum and maximum of HUE in HSV
+    minH = cv2.getTrackbarPos('lowH', 'HSV')
+    maxH = cv2.getTrackbarPos('highH', 'HSV')
+
+    #Get the trackbars position for the minimum and maximum of SATURATION in HSV
+    minS = cv2.getTrackbarPos('lowS', 'HSV')
+    maxS = cv2.getTrackbarPos('highS', 'HSV')
+    
+    #Get the trackbars position for the minimum and maximum of  VALUE in HSV
+    minV = cv2.getTrackbarPos('lowV', 'HSV')
+    maxV = cv2.getTrackbarPos('highV', 'HSV')
+    
+    # Convert the video from BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Find the inRange values of HSV to obtain Black and White scaled image 
+    min_hsv = np.array([minH, minS, minV])
+    max_hsv = np.array([maxH, maxS, maxV])
+    scaled = cv2.inRange(hsv, min_hsv, max_hsv)
+    
+    #Show the HSV image with the adjusted range of values
+    hsv = cv2.bitwise_and(hsv,hsv, mask = scaled)
+
+    #Dilate the scaled image 
+    scaled = cv2.dilate(scaled, None, iterations = 2) 
+    
+    cv2.imshow("Video",frame) #display original video
+    cv2.imshow("HSV",hsv) #display video in HSV
+    cv2.imshow("Black+White", scaled) #display the black+white scaled version of the hsv video
+
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+# Release the capture and destroy all the windows when program ends
+cap.release()
+cv2.destroyAllWindows()
